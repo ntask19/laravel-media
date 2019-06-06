fd0a1a1a7f9061789f8ee092b5e1e116095441a5
Task Nagashige <task.19.xx@gmail.com>
2019-06-07 01:01:49 +0900

    タスク作成機能の追加

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,35 +1,19 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ToDo App</title>
-    <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-<header>
-    <nav class="my-navbar">
-        <a class="my-navbar-brand" href="/">ToDo App</a>
-    </nav>
-</header>
-<main>
+@extends('layout')
+
+@section('content')
     <div class="container">
         <div class="row">
             <div class="col col-md-4">
                 <nav class="panel panel-default">
                     <div class="panel-heading">フォルダ</div>
                     <div class="panel-body">
-                        <a href="#" class="btn btn-default btn-block">
-                            <a href="{{ route('folders.create') }}" class="btn btn-default btn-block">
-                                フォルダを追加する
-                            </a>
+                        <a href="{{ route('folders.create') }}" class="btn btn-default btn-block">
+                            フォルダを追加する
                         </a>
                     </div>
                     <div class="list-group">
                         @foreach($folders as $folder)
-                            <a
-                                href="{{ route('tasks.index', ['id' => $folder->id]) }}"
+                            <a href="{{ route('tasks.index', ['id' => $folder->id]) }}"
                                 class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : '' }}">
                                 {{ $folder->title }}
                             </a>
@@ -43,7 +27,9 @@
                     <div class="panel-body">
                         <div class="text-right">
                             <a href="#" class="btn btn-default btn-block">
-                                タスクを追加する
+                                <a href="{{ route('tasks.create', ['id' => $current_folder_id]) }}" class="btn btn-default btn-block">
+                                    タスクを追加する
+                                </a>
                             </a>
                         </div>
                     </div>
@@ -61,9 +47,9 @@
                             <tr>
                                 <td>{{ $task->title }}</td>
                                 <td>
-                                    <span class="label">{{ $task->status }}</span>
+                                    <span class="label {{ $task->status_class }}">{{ $task->status_label }}</span>
                                 </td>
-                                <td>{{ $task->due_date }}</td>
+                                <td>{{ $task->formatted_due_date }}</td>
                                 <td><a href="#">編集</a></td>
                             </tr>
                         @endforeach
@@ -73,6 +59,4 @@
             </div>
         </div>
     </div>
-</main>
-</body>
-</html>
+@endsection
